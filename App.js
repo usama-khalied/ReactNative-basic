c68516bfb6b5cbdc13cfb8e7e85bbe1eb6f72e18
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-04-04 12:18:01 +0500

    Update App.js

--- App.js
+++ App.js
@@ -2,59 +2,31 @@ import React, { useState } from 'react';
 import { SafeAreaView, StyleSheet, Button, Alert, StatusBar, TouchableOpacity, Platform, TouchableWithoutFeedback, TouchableNativeFeedback, View, Image, Text } from "react-native";
 
 
-
-
+const STYLES = ['default', 'dark-content', 'light-content'];
+const TRANSITION = ['fade', 'slide', 'none'];
 
 export default function App() {
-  // console.log(require('./assets/icon.png'));
-  const [count, setcount] = useState(0);
-  const onPress = () => {
-    setcount(count + 1);
+  const [hidden, sethidden] = useState(false);
+  const [statusbarstyle, setsatusbarstyle] = useState(STYLES[0]);
+  const [statusbartransition, setstatusbartransition] = useState(TRANSITION[0]);
+
+  changeStatusbarTransition = () => {
+
 
   }
 
-  const containerStyle = { backgroundColor: "green" };
-  const styleing1 = { margin: 12 }
+
+
 
   return (
 
-    <SafeAreaView style={[styles.container2, containerStyle]}>
-
-      {/* <Image style={styles.img} source={require('./assets/splash.png')} /> */}
-      <Text> Hellow Count :  {count} </Text>
-      <TouchableOpacity onPress={onPress}>
-        <Image
-          source={{
-            uri: "https://picsum.photos/200/300?grayscale",
-            width: 300,
-            height: 400,
-          }} />
-      </TouchableOpacity>
-      <View style={styles.profile}>
-
-
-        <Button color="purple" title='alert' onPress={() => {
-          setcount(count + 1), Alert.alert("My title", "bhai kiya kr rahey ho ", [
-            { text: "Done" },
-            { text: "Cancel" },
-          ])
-        }} />
-
-
-        <Button title='Check' color="red" onPress={() => {
-          Alert.alert("Confirmation", "Are you permission ", [
-
-            { text: "Done" },
-            { text: "Cancel" },
-          ])
-        }} />
-        <Button title='new' onPress={() => Alert.alert("Confirmation", "confirm", [
-          { text: "Done" },
-          { text: "Cancel" }
-
-        ])} />
-        <Button title='Click me' onPress={() => Alert.prompt("WHAT", "NAME", text => alert(text))} />
-
+    <SafeAreaView style={styles.container}>
+      <StatusBar animated={true} backgroundColor="#61dafb"
+      />
+      <Text style={styles.textStyle}> </Text>
+      <Text style={styles.textStyle}> </Text>
+      <View style={styles.buttonsContainer}>
+        <Button onPress={changeStatusbarTransition} title='toggleStatusbar' />
       </View>
     </SafeAreaView>
   );
@@ -64,24 +36,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    // paddingTop: Platform.OS === 'android' ? 20 : 0,
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    justifyContent: 'center',
+    alignItems: 'center'    // paddingTop: Platform.OS === 'android' ? 20 : 0,
+    // paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
 
   },
-  container2: {
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  buttonsContainer: {
+    padding: 10,
   },
-  profile: {
-    flex: 0,
-    flexDirection: 'row',
-    width: 100,
-  },
-  img: {
-    height: 150,
-    width: 150,
-  },
-  btn: {
-    backgroundColor: 'red',
-    color: '#000',
+  textStyle: {
+    textAlign: 'center',
+    marginBottom: 8,
+
   }
 });
+
+
+
+
+
+
+
