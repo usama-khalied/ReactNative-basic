048019c1703929401192b7ceacec82f7e2f731d4
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-04-15 12:41:29 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet, Button, Alert, StatusBar, Dimensions, useWindowDimensions, TouchableOpacity, Platform, TouchableWithoutFeedback, TouchableNativeFeedback, View, Image, Text } from "react-native";
 import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
-import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 
 // const TRANSITION = ['fade', 'slide', 'none'];
@@ -49,9 +48,9 @@ export default function App() {
 
         <View style={{
           backgroundColor: 'red',
-          width: 200,
-          //flexBasis: 100,
-          //flexGrow: 1,
+          // width: 200,
+          flexBasis: 200,
+          // flexShrink: 1,
           flex: -1,
           height: 100,
         }}>
@@ -60,12 +59,14 @@ export default function App() {
           backgroundColor: 'blue',
           width: 100,
           height: 100,
+
         }}>
         </View>
         <View style={{
           backgroundColor: 'orange',
           width: 100,
           height: 100,
+          // flexShrink: 1,
         }}>
         </View>
       </View>
