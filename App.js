9cb43b3b9244d84eb79a73292b1184bea03c7cd1
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-04-18 14:28:35 +0500

    Update App.js

--- App.js
+++ App.js
@@ -46,35 +46,8 @@ export default function App() {
 
       }}>
 
-        <View style={{
-          backgroundColor: 'red',
-          // width: 200,
-          flexBasis: 100,
-          // flexShrink: 1,
-          // flex: -1,
-          height: 100,
 
-          //bottom: 20,
 
-        }}>
-        </View>
-        <View style={{
-          backgroundColor: 'blue',
-          width: 100,
-          height: 100,
-          top: 20,
-          left: 30,
-          position: 'relative',
-          // zIndex: 1,
-        }}>
-        </View>
-        <View style={{
-          backgroundColor: 'orange',
-          width: 100,
-          height: 100,
-
-        }}>
-        </View>
       </View>
 
 
