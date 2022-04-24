e3f66be4ef1ae6fd847656eb729ec673b6afd9d1
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-04-24 08:10:00 -0700

    Update AppText.js

--- Components/AppText.js
+++ Components/AppText.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Text , StyleSheet , Platform } from 'react-native';
-function AppText({children}) {
+function AppText(props) {
     return (
-        // <Text style={styles.beech}> {children} </Text>
+      
         <>
         </>
     );
 }
 const styles = StyleSheet.create({
-  beech : {
-      fontSize:18,
-      fontFamily : Platform.OS === 'android' ? " Roboto" : "Avenir"   }  
+ 
 })
 export default AppText;
