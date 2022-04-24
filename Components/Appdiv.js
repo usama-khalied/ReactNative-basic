d3d1c6900b0f17e01142e023ec7f390ad4989d50
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-04-24 08:09:54 -0700

    Update Appdiv.js

--- Components/Appdiv.js
+++ Components/Appdiv.js
@@ -2,17 +2,11 @@ import React from 'react';
 import { Text , StyleSheet  , View , Platform } from 'react-native';
 function Appdiv({children}) {
     return (
-        // <View style={styles.beech1}> {children} </View>
-        <>
-        </>
+<Text style={{fontSize:20,color:'red'}}> {children} </Text>
     );
 }
 const styles = StyleSheet.create({
-//   deev1 : {
-//       height: 200,
-//       backgroundColor : 'blue',
-//       width : Platform.OS === 'android' ? 200 : 100
 
-//   }  
+
 })
 export default Appdiv;
