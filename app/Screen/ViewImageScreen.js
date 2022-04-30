968d16a63d02d9423072976c994030d9aad25521
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-04-30 08:09:50 -0700

    Update ViewImageScreen.js

--- app/Screen/ViewImageScreen.js
+++ app/Screen/ViewImageScreen.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import {ImageBackground , View , Image , Text , StyleSheet} from 'react-native'
 
+
+import color from '../config/color'
+
+
+
+
 function ViewImageScreen(props) {
     return (
 <>
 
 <View style={styles.container}>
 <View style={styles.div}>  </View>
+<View style={styles.div1}>  </View>
+
 <Image style={styles.img} source={require('../assets/wallpaper.jpg')}/>
 </View>
 </>
@@ -14,8 +22,21 @@ function ViewImageScreen(props) {
 }
 const styles = StyleSheet.create({
     div : {
-
+height: 50,
+width: 50,
+position : 'absolute',
+top : 40,
+left: 30,
+backgroundColor :color.primary,
     },
+    div1: {
+        height: 50,
+        width: 50,
+        position : 'absolute',
+        top : 40,
+        right: 30,
+        backgroundColor : color.secondary,
+            },
     img : {
         resizeMode: "contain",
         width: "100%",
@@ -23,7 +44,7 @@ const styles = StyleSheet.create({
 
     },
     container : {
-    backgroundColor: "pink",
+    backgroundColor: "white",
     flex :1,
     
     },
