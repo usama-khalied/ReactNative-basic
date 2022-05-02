461a1d94d52e97fe8326b5d18f809abbbfc03da8
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-05-02 13:10:42 -0700

    Update ViewImageScreen.js

--- app/Screen/ViewImageScreen.js
+++ app/Screen/ViewImageScreen.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import {ImageBackground , View , Image , Text , StyleSheet} from 'react-native'
+import React from 'react'
+import { View , Image ,  Text , Platform , StyleSheet} from 'react-native'
+
+
 
 
 import color from '../config/color'
@@ -7,47 +9,17 @@ import color from '../config/color'
 
 
 
-function ViewImageScreen(props) {
+function ViewImageScreen({children}) {
     return (
 <>
+<Text style={styles.sty2}>{children}</Text>
 
-<View style={styles.container}>
-<View style={styles.div}>  </View>
-<View style={styles.div1}>  </View>
-
-<Image style={styles.img} source={require('../assets/wallpaper.jpg')}/>
-</View>
 </>
     );
 }
 const styles = StyleSheet.create({
-    div : {
-height: 50,
-width: 50,
-position : 'absolute',
-top : 40,
-left: 30,
-backgroundColor :color.primary,
-    },
-    div1: {
-        height: 50,
-        width: 50,
-        position : 'absolute',
-        top : 40,
-        right: 30,
-        backgroundColor : color.secondary,
-            },
-    img : {
-        resizeMode: "contain",
-        width: "100%",
-        height:"100%",
-
-    },
-    container : {
-    backgroundColor: color.white,
-    flex :1,
-    
-    },
-
+    sty2 : {
+        fontSize:50,
+    }
 })
 export default ViewImageScreen;
